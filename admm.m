5889dc0855ec101edadfee4f1ab548e24dd2be9c
Shyam Upadhyay <shyamupa@gmail.com>
2014-05-15 16:41:19 -0500

    save

--- admm.m
+++ admm.m
@@ -3,12 +3,12 @@ function [w,v]=admm(docs,y,M,X,lambda_sen,lambda_las,rho)
 [corpus_size,vocab_size]=size(M);
 
 % bias
-X = [X, ones(length(docs),1)]; 
-vocab_size = vocab_size+1;
-M = [M, ones(corpus_size,1)];
+% X = [X, ones(length(docs),1)]; 
+% vocab_size = vocab_size+1;
+% M = [M, ones(corpus_size,1)];
 
 N = sum(M(:,:));  % Ni is the sum of ith column of M, number of times ith word in vocab appears in the corpus
-
+N = N';
 
 w=zeros(vocab_size,1);
 
@@ -37,42 +37,53 @@ lambdaVect = lambda*[0;ones(size(X, 2)-1,1)];
 iter = 1;
 t =[];
 
+history(MAXITER).r=[];
+history(MAXITER).s=[];
+history(MAXITER).eps_primal=[];
+history(MAXITER).eps_dual=[];
 while(iter <= MAXITER)
     tic;
-
     MyMu=updateMu(v,u,N,M,rho);
-    
+    time=toc;
+    fprintf('Time taken for Mu Update %f \n',time);
+
+    tic;
     funObj = @(w)lassoObj(w,v,X,y,M,rho,u,N,MyMu);
     w = L1General2_SPG(funObj,w,lambdaVect,gOptions);
+    time=toc;
+    fprintf('Time taken for W Update %f \n',time);
     
+    tic;
     oldV=v;
     for d=1:length(docs)
-        for i=1:length(docs(d).sent_offsets)
+        parfor i=1:length(docs(d).sent_offsets)
             z=M(docs(d).sent_offsets(i),:)*w - u(docs(d).sent_offsets(i))./rho; % z_(d,s)
-
             v(docs(d).sent_offsets(i))=updateV(lambda_sen,rho,z); % v_(d,s)
         end
     end
+    time=toc;
+    fprintf('Time taken for V Update %f \n',time);
+    break;
     
     u = u + rho*(v-M*w);
     iter = iter + 1;
     
     r = v-M*w;
-    s = rho*M*(v-oldV);
+    s = rho*M'*(v-oldV);
     
     eps_primal = ABSTOL*sqrt(corpus_size) + RELTOL*max([norm(M*w),norm(v)]);
     eps_dual = ABSTOL*sqrt(corpus_size) + RELTOL*max([norm(M*w),norm(v)]);
     
     if norm(r) < eps_primal && norm(s) < eps_dual
         break
+    else
+        history(iter).r = norm(r);
+        history(iter).s = norm(s);
+        history(iter).eps_primal = eps_primal;
+        history(iter).eps_dual = eps_dual;
     end
-    rho=updateRho(r,s,eps_primal,eps_dual);
-    disp(['Obj is ',num2str(calcObj(v,w,X,y,M,docs,rho))]);
-    disp(['iter ', num2str(iter)]);
-    
-    toc;
-end
 
-% save('w_hail_mary', 'w')
+    fprintf('Obj is %f, r is %f e_p is %f, s is %f e_d is %f \n',calcObj(v,w,X,y,M,docs,rho),norm(r),eps_primal,norm(s),eps_dual);
+end
 
 end
--- lassoObj.m
+++ lassoObj.m
@@ -3,7 +3,7 @@ function [obj, g]=lassoObj(w,v,X,y,M,rho,u,N,MyMu)
 
 tmp = rho/2*norm(M*w-(v+ (u./rho))).^2;
 obj = obj + tmp; 
-g = g + rho.*N'.*(w-MyMu);
+g = g + rho.*N.*(w-MyMu);
 
 
 
