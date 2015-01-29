557001271d0ead2ec2f05ab18b3669802ebd0d1e
Shyam Upadhyay <shyamupa@gmail.com>
2015-01-29 17:11:14 -0600

    bug

--- predict.m
+++ predict.m
@@ -4,8 +4,8 @@ sig = 1./(1+exp(-Xw))
 sig(sig>0.5)=1;
 sig(sig<0.5)=-1;
 p=sig;
-error=abs(sum(p-y));
-total=size(y);
+error=abs(sum(p-y))
+total=size(y,1);
 fprintf('error %f out of %f \n',error,total);
 
 
