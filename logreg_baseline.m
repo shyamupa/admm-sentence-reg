1d70ddf3d80401e9c753054ed69c6646f2494549
Shyam Upadhyay <shyamupa@gmail.com>
2015-01-29 17:26:49 -0600

    no regularization

---
+++ logreg_baseline.m
@@ -0,0 +1,11 @@
+function [wout]=logreg_baseline(X,y)
+% log reg with no regularization
+[nInst,nVars]= size(X);
+
+w_init = zeros(nVars,1);
+funObj = @(w)LogisticLoss(w,X,y);
+
+fprintf('\nRunning Scaled Conjugate Gradient\n');
+options.Method = 'scg';
+options.MAXITER = 100;
+[wout,~,~,~]=minFunc(@LogisticLoss,w_init,options,X,y);
