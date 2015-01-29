8d517f52ef194c431d1c38dc51859f3071120697
Shyam Upadhyay <shyamupa@gmail.com>
2015-01-29 17:33:08 -0600

    testing

--- demo.m
+++ demo.m
@@ -8,14 +8,14 @@ sparsity= @(w) ( sum((w==0)) );
 X = [X, ones(size(X,1),1)]; % one more col to X
 y(y==0)=-1; % important!
 M = [M, ones(size(M,1),1)]; % one more col to M
-
+% 
 % wout=logreg_baseline(X,y);
 % predict(X,y,wout);
-% fprintf('sparsity %f\n',sparsity(wout));
+% fprintf('sparsity %f out of %f\n',sparsity(wout),size(wout,1));
 % return 
 lambda_sen = 0.001; 
 lambda_las = 0.1; 
 rho = 1; 
 [w,history]=admm(docs,y,M,X,lambda_sen,lambda_las,rho); 
 predict(X,y,w);
-fprintf('sparsity %f\n',sparsity(w));
+fprintf('sparsity %f\n',sparsity(w),size(w,1));
