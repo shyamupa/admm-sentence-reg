1792760c23681cdc0f0c9b7f9f25120c19208c0c
Shyam Upadhyay <shyamupa@gmail.com>
2015-05-08 15:28:42 -0500

    sve

--- predict.m
+++ predict.m
@@ -1,10 +1,10 @@
 function [p]=predict(X,y,w)
 Xw = X*w;
-sig = 1./(1+exp(-Xw))
+sig = 1./(1+exp(-Xw));
 sig(sig>0.5)=1;
 sig(sig<0.5)=-1;
 p=sig;
-error=abs(sum(p-y))
+error=abs(sum(p-y));
 total=size(y,1);
 fprintf('error %f out of %f \n',error,total);
 
